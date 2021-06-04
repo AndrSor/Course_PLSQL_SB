66d0788b41438daca41b7e44c662d0b01c392280
Andrey Sorokolat <38098815+AndrSor@users.noreply.github.com>
2021-06-04 12:06:53 +0300

    Add files via upload

---
+++ Dom.Автоинкрементное поле IDENTITY.sql
@@ -0,0 +1,15 @@
+﻿ALTER SESSION SET CURRENT_SCHEMA=c##course;
+
+DROP TABLE test1;
+
+CREATE TABLE test1 (
+     id             NUMBER GENERATED ALWAYS AS IDENTITY
+    ,acct           NUMBER
+    ,name           VARCHAR(100));
+    
+INSERT INTO test1 (acct,name) VALUES (1,'Андрей');
+INSERT INTO test1 (acct,name) VALUES (5,'Илья');
+
+SELECT * FROM test1;
+
+DROP TABLE test1;
