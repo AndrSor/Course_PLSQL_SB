85217dd221867d1d49a1b65c745ced37a95a0e48
Andrey Sorokolat <38098815+AndrSor@users.noreply.github.com>
2021-06-04 10:09:02 +0300

    Add files via upload

---
+++ Course Work/CourseWork.01.Create_table_CLIENT.sql
@@ -0,0 +1,37 @@
+﻿ALTER SESSION SET CURRENT_SCHEMA=c##course;
+
+------
+------ Удаление таблицы CLIENTS
+------
+
+DROP TABLE client;
+
+------
+------ Создание таблицы клиенты (физ. лица)
+------
+
+CREATE TABLE client (
+    id          NUMBER,
+    cl_name     VARCHAR2(100),
+    date_birth  DATE
+);
+
+------
+------ Создание индекса
+------
+
+CREATE UNIQUE INDEX idx_client_id
+    ON client (id);
+
+------
+------ Добавление первичного ключа
+------
+
+ALTER TABLE client
+    ADD
+    ( 
+        CONSTRAINT client_id_pk
+       		 PRIMARY KEY (id)
+    );
+
+
---
+++ Course Work/CourseWork.02.Create_table_PR_CREDIT.sql
@@ -0,0 +1,56 @@
+﻿ALTER SESSION SET CURRENT_SCHEMA=c##course;
+
+------
+------ Удаление таблицы PR_CREDIT
+------
+
+DROP TABLE pr_credit;
+
+------
+------ Создание таблицы кредитные договоры
+------
+
+CREATE TABLE pr_credit (
+      id              NUMBER
+    , num_dog         VARCHAR2(10)
+    , summa_dog       NUMBER(*,2)
+    , date_begin      DATE
+    , date_end        DATE
+    , id_client       NUMBER
+    , collect_plan    NUMBER
+    , collect_fact    NUMBER
+    , CONSTRAINT      collect_plan_unique   UNIQUE (collect_plan)
+    , CONSTRAINT      collect_fact_unique   UNIQUE (collect_fact)
+    
+);
+
+------
+------ Создание индексов
+------
+
+CREATE UNIQUE INDEX idx_pr_credit_id
+    ON pr_credit (id);
+--CREATE UNIQUE INDEX idx_pr_credit_collect_plan
+--    ON pr_credit (collect_plan);
+--CREATE UNIQUE INDEX idx_pr_credit_collect_fact
+--    ON pr_credit (collect_fact);
+
+
+------
+------ Добавление первичного и внешних ключей
+------
+
+
+ALTER TABLE pr_credit
+    ADD
+    ( 
+          CONSTRAINT    pr_credit_id_pk
+                            PRIMARY KEY (id,collect_plan,collect_fact)
+        , CONSTRAINT    pr_credit_id_client_fk
+                            FOREIGN KEY (id_client)
+                                REFERENCES client(id)
+
+    );
+
+
+
